34f34504ea0a627f5de71724285103029f6e17b9
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-01-28 16:39:50 -0800

    complete and styled search component

--- src/modules/Search.jsx
+++ src/modules/Search.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { searchCoin } from '../services/cryptoApi';
 import { Circles } from 'react-loader-spinner';
 
+import styles from './Search.module.css';
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState('');
   const [coins, setCoins] = useState([]);
@@ -36,14 +37,14 @@ function Search({ currency, setCurrency }) {
     return () => controller.abort();
   }, [text]);
   return (
-    <div>
+    <div className={styles.searchBox}>
       <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div>
+      <div className={coins.length > 0 ? styles.searchResult : null}>
         {isLoading && <Circles width="50px" height="50px" color="#e9f817" />}
         <ul>
           {coins.map((coin) => (
